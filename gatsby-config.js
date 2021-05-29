4a848d644dde8709797c4aa0c4895e90e0ab5adf
Mandjo Béa Boré <37101160+mandjo2010@users.noreply.github.com>
2021-05-29 12:14:11 +0200

    cloud2

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,22 +10,13 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-gatsby-cloud`,
     {
-      resolve: [
-        `gatsby-source-datocms`, 
-        `gatsby-plugin-gatsby-cloud`,
-      ],
+      resolve: `gatsby-source-datocms`,
+      
       options: {
         apiToken: process.env.DATO_API_TOKEN,
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-    
+        
+        
       },
     },
   ],
 }
-
